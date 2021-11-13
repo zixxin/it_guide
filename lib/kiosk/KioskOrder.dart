769037698b8178a649e05277f7e847795bf6612e
healimfx <22000770@handong.edu>
2021-11-13 15:02:44 +0900

    Create KioskOrder.dart

---
+++ lib/kiosk/KioskOrder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KioskOrder extends StatefulWidget {
+  const KioskOrder({ Key? key }) : super(key: key);
+
+  @override
+  _KioskOrderState createState() => _KioskOrderState();
+}
+
+class _KioskOrderState extends State<KioskOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
