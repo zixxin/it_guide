641f09acb29cde76131cf0fa03b33954bf136367
healimfx <22000770@handong.edu>
2021-11-17 14:08:50 +0900

    text 추가

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.title, 
+          widget.title,
           style: TextStyle(
-            // color: Colors.yellow[700],
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold
-          ),
+              // color: Colors.yellow[700],
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold),
         ),
       ),
       body: Center(
@@ -55,49 +54,46 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Image.asset("img/banner.png"),
               // child: Text('2')
             ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
+            Row(children: <Widget>[
+              Expanded(
+                child: Container(
                     padding: EdgeInsets.all(50.0),
                     color: Colors.cyan,
-                    child: Text('ZOOM')
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox (
-                    width: 120,
-                    height: 120,
-                    child: ElevatedButton (
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => KioskMain(title: 'Kiosk')),
-                        );
-                      },
-                      child: Text(
-                        '키오스크',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    child: Text('ZOOM')),
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => KioskMain(title: '키오스크')),
+                      );
+                    },
+                    child: Text(
+                      '키오스크',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                      style: ButtonStyle(
-                        backgroundColor: // Colors.blue;
-                            MaterialStateProperty.all <Color>(Colors.pink),
-                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: // Colors.blue;
+                          MaterialStateProperty.all<Color>(Colors.pink),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                  padding: EdgeInsets.all(50.0),
-                  color: Colors.amber,
-                  child: Text('카드결제')
-                  ),
-                )
-              ]
-            )
+              ),
+              Expanded(
+                child: Container(
+                    padding: EdgeInsets.all(50.0),
+                    color: Colors.amber,
+                    child: Text('카드결제')),
+              )
+            ])
           ],
         ),
       ),
