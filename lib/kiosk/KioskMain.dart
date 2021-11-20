3b64ea0c38c3cb937a3aa80b4caa793903ef33d5
healimfx <22000770@handong.edu>
2021-11-21 04:36:54 +0900

    Update KioskMain.dart

--- lib/kiosk/KioskMain.dart
+++ lib/kiosk/KioskMain.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:it_guide/kiosk/KioskFood.dart';
+import 'package:it_guide/kiosk/ThreeFood.dart';
+import 'package:it_guide/main.dart';
 
 class KioskMain extends StatefulWidget {
   KioskMain({Key? key, required this.title}) : super(key: key);
@@ -29,7 +30,7 @@ class _kiosk_mainState extends State<KioskMain> {
                 child: Container(
                     height: 100,
                     decoration: BoxDecoration(
-                      color: Colors.red[600],
+                      color: Colors.red[700],
                       border: Border.all(color: Colors.black, width: 1),
                     ),
                     child: Container(
@@ -37,6 +38,7 @@ class _kiosk_mainState extends State<KioskMain> {
                         child: Text(
                           '버거',
                           style: TextStyle(
+                            color: Colors.white,
                             fontSize: 35.0,
                             fontWeight: FontWeight.bold,
                           ),
@@ -49,7 +51,6 @@ class _kiosk_mainState extends State<KioskMain> {
                       border: Border.all(color: Colors.black),
                     ),
                     height: 100,
-                    // color: Colors.green,
                     child: Container(
                         margin: EdgeInsets.only(top: 30),
                         child: Text(
@@ -65,8 +66,6 @@ class _kiosk_mainState extends State<KioskMain> {
                       border: Border.all(color: Colors.black),
                     ),
                     height: 100,
-                    // padding: EdgeInsets.all(40.0),
-                    // color: Colors.purple,
                     child: Container(
                         margin: EdgeInsets.only(top: 30),
                         child: Text(
@@ -82,11 +81,9 @@ class _kiosk_mainState extends State<KioskMain> {
                 Expanded(
                     child: Container(
                         height: 150,
-                        // padding: EdgeInsets.all(120.0),
-                        // color: Colors.pinkAccent,
                         child: ElevatedButton(
                           style: ButtonStyle(
-                            backgroundColor: // Colors.blue;
+                            backgroundColor:
                                 MaterialStateProperty.all<Color>(Colors.white),
                           ),
                           onPressed: () {
@@ -94,7 +91,7 @@ class _kiosk_mainState extends State<KioskMain> {
                               context,
                               MaterialPageRoute(
                                   builder: (context) =>
-                                      KioskFood(title: 'Kiosk')),
+                                      ThreeFood(title: "키오스크")),
                             );
                           },
                           child: Image.asset("img/burger1.png"),
@@ -102,19 +99,40 @@ class _kiosk_mainState extends State<KioskMain> {
                 Expanded(
                     child: Container(
                   height: 150,
-                  // padding: EdgeInsets.all(120.0),
-                  // color: Colors.pinkAccent,
                   child: Image.asset("img/burger2.png"),
-                  // child: Text('2')
                 )),
               ],
             ),
-            Positioned(child: _text1()),
             Container(
               height: 150,
             ),
+            FloatingActionButton(
+                onPressed: () => showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) {
+                      return AlertDialog(
+                          content: const Text(
+                            "안녕하세요 마루에요! \n지금부터 메뉴 주문을 위한\n키오스크 사용법을 알려줄게요! \n 먼저 주문할 버거 세트 버튼을 \n터치해주세요!",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('확인'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ]);
+                    }),
+                child: Image.asset('img/maru.png')),
             Container(
-                height: 220,
+              height: 10,
+            ),
+            Container(
+                height: 175,
                 color: Colors.grey[300],
                 child: Container(
                     margin: const EdgeInsets.all(20),
@@ -127,57 +145,47 @@ class _kiosk_mainState extends State<KioskMain> {
               children: [
                 Expanded(
                   child: Container(
-                      height: 120,
-                      // padding: EdgeInsets.all(120.0),
-                      color: Colors.grey[400],
-                      child: Container(
-                          margin: EdgeInsets.only(top: 40),
-                          child: Text(
-                            '취소',
-                            style: TextStyle(
-                                fontSize: 35.0, fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          ))),
+                    height: 100,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey.shade400),
+                        ),
+                        onPressed: () {},
+                        child: Container(
+                            child: Text(
+                          '취소',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 35.0,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ))),
+                  ),
                 ),
                 Expanded(
                   child: Container(
-                      height: 120,
-                      // padding: EdgeInsets.all(120.0),
-                      color: Colors.grey[500],
-                      child: Container(
-                          margin: EdgeInsets.only(top: 40),
-                          child: Text(
-                            '결제하기',
-                            style: TextStyle(
-                                fontSize: 35.0, fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          ))),
-                ),
+                    height: 100,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey.shade500),
+                        ),
+                        onPressed: () {},
+                        child: Container(
+                            child: Text(
+                          '결제하기',
+                          style: TextStyle(
+                              fontSize: 35.0, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ))),
+                  ),
+                )
               ],
-            )
+            ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _text1() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: EdgeInsets.all(10),
-          color: Colors.blue[300],
-          child: Text(
-            '원하는 버거의 사진을 클릭하여\n카트에 넣어보세요!',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
-              color: Colors.black,
-            ),
-          ),
-        )
-      ],
     );
   }
 }
